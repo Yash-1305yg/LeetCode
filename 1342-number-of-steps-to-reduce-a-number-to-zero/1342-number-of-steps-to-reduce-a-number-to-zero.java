aa57b5ede586b89e360a775aa9a9931ca824adb2
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-07 08:46:22 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (17.20%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSteps(int n) {
+        int steps =0;
+        while(n>0){
+            if(n %2==0){
+                n=n/2;
+                steps++;
+            }
+            else{
+                n=n-1;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
