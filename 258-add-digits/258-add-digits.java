14ffede193c96f15aeee2bec1ed609a6ef55b37c
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-08 07:43:10 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (17.98%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -2,17 +2,26 @@ class Solution {
     public int addDigits(int num) {
         int sum =0;
         
-        while(num >0)
-        {
-            int temp = num;
-            temp = temp%10;
-            sum =sum+temp;
-            num=num/10;
+        if(num ==0)
+            return 0;
+        
+        if(num%9==0)
+            return 9;
+        
+        else{
+            return num%9;
         }
-        if(sum >9)
-        {
-            return addDigits(sum);
-        }
-        return sum;
+        // while(num >0)
+        // {
+        //     int temp = num;
+        //     temp = temp%10;
+        //     sum =sum+temp;
+        //     num=num/10;
+        // }
+        // if(sum >9)
+        // {
+        //     return addDigits(sum);
+        // }
+        // return sum;
     }
 }
