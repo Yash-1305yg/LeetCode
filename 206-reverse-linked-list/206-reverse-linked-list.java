4ecd049cf277fde03b1191eca72d2cd8b6a71ab2
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-15 19:46:18 +0530

    Time: 0 ms (100.00%), Space: 43 MB (12.18%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,21 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode temp = null;
-            ListNode current = head;
-            ListNode prev = null;
-        if(head != null )
-        {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode temp=null;
+        while(current!=null){
+            temp=current.next;
+            current.next=prev;
+            prev=current;
+            current=temp;
             
-            
-            while(current != null)
-            {
-                temp = current.next;
-                current.next = prev;
-                prev = current ; 
-                current = temp;
-            }
         }
-       return prev;
+        return prev;
     }
 }
