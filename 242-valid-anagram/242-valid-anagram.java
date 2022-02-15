e9f730f8dd3fbaac8ead93ab1e839acc4f35770a
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-15 21:41:01 +0530

    Time: 17 ms (22.84%), Space: 47 MB (5.27%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,18 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
+//         char[] s1 = s.toCharArray();
+//         char[] s2 = t.toCharArray();
         
-        Arrays.sort(s1);
-        Arrays.sort(s2);
-        if(s1.length != s2.length)
+//         Arrays.sort(s1);
+//         Arrays.sort(s2);
+        if(s.length() != t.length())
             return false;
-        for(int i =0; i< s1.length;i++)
+//         for(int i =0; i< s1.length;i++)
+//         {
+//             if(s1[i] != s2[i])
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int m = t.length();
+        
+        for(int i =0;i< n;i++)
         {
-            if(s1[i] != s2[i])
-            {
+            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) ,0) +1);
+        }
+        for(int i =0;i< m;i++)
+        {
+            map.put(t.charAt(i) , map.getOrDefault(t.charAt(i) ,0) -1);
+        }
+        
+        for(int i=0 ; i< n ;i++)
+        {
+            if(map.get(s.charAt(i)) !=0)
                 return false;
-            }
         }
         return true;
     }
