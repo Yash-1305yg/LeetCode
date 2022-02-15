5d03ca7a77d8277622578d5a6be2b502db149718
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-15 21:42:47 +0530

    Time: 6 ms (57.66%), Space: 44.8 MB (11.91%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,39 +1,46 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-//         char[] s1 = s.toCharArray();
-//         char[] s2 = t.toCharArray();
         
-//         Arrays.sort(s1);
-//         Arrays.sort(s2);
-        if(s.length() != t.length())
+        ////////////// METHOD 1//////////////
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+        
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        if(s1.length != s2.length)
             return false;
-//         for(int i =0; i< s1.length;i++)
-//         {
-//             if(s1[i] != s2[i])
-//             {
-//                 return false;
-//             }
-//         }
-//         return true;
-        
-        HashMap<Character, Integer> map = new HashMap<>();
-        int n = s.length();
-        int m = t.length();
-        
-        for(int i =0;i< n;i++)
+        for(int i =0; i< s1.length;i++)
         {
-            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) ,0) +1);
-        }
-        for(int i =0;i< m;i++)
-        {
-            map.put(t.charAt(i) , map.getOrDefault(t.charAt(i) ,0) -1);
-        }
-        
-        for(int i=0 ; i< n ;i++)
-        {
-            if(map.get(s.charAt(i)) !=0)
+            if(s1[i] != s2[i])
+            {
                 return false;
+            }
         }
         return true;
+        
+        
+        ///////////// METHOD 2///////////
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         int n = s.length();
+//         int m = t.length();
+        
+//         if(s.length() != t.length())
+//             return false;
+        
+//         for(int i =0;i< n;i++)
+//         {
+//             map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) ,0) +1);
+//         }
+//         for(int i =0;i< m;i++)
+//         {
+//             map.put(t.charAt(i) , map.getOrDefault(t.charAt(i) ,0) -1);
+//         }
+        
+//         for(int i=0 ; i< n ;i++)
+//         {
+//             if(map.get(s.charAt(i)) !=0)
+//                 return false;
+//         }
+//         return true;
     }
 }
