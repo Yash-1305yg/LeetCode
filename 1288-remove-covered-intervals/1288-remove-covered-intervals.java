8995c05fc617df6c4529a2a7898d656b2e581148
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-20 18:12:09 +0530

    Time: 9 ms (50.55%), Space: 46 MB (26.37%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeCoveredIntervals(int[][] in) {
         int res=0 ,left=-1 , right=-1;
-        Arrays.sort(in, (a, b) -> a[0] - b[0]); // to sort the arrays accoring to their first element
+        Arrays.sort(in, (a, b) -> a[0] - b[0]); // to sort the arrays accoring to their first element it is using lamba expression if the result is -1 then b i bigger else a is bigger
         for(int[] arr : in)
         {
             if(arr[0]>left && arr[1]>right)
