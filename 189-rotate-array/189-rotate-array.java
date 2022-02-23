befa42296ee8607968813f0cb543a7f079958981
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-23 09:46:23 +0530

    Time: 0 ms (100.00%), Space: 64.9 MB (9.78%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,19 +1,40 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(k==0)
-            return;
-        if(nums==null || nums.length==0)
-            return;
-        int[] res = new int[nums.length];
+//         /// 1st method naive soltuion///
+//         if(k==0)
+//             return;
+//         if(nums==null || nums.length==0)
+//             return;
+//         int[] res = new int[nums.length];
         
-        for(int i=0;i<nums.length;i++)
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             int newindex = (i+k) %nums.length;
+//             res[newindex] = nums[i];
+//         }
+//         for(int i=0  ;i<nums.length;i++)
+//         {
+//             nums[i]=res[i];
+//         }
+        
+        ///2nd method////
+        
+        k=k%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+        
+    }
+    
+    public void reverse(int[] nums , int start,int end)
+    {
+        while(start<end)
         {
-            int newindex = (i+k) %nums.length;
-            res[newindex] = nums[i];
-        }
-        for(int i=0  ;i<nums.length;i++)
-        {
-            nums[i]=res[i];
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
