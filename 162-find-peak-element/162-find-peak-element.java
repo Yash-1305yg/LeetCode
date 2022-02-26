64ebb10fe4e74dc0dfe004c92aec480c70dbfbd4
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-26 10:57:59 +0530

    Time: 0 ms (100.00%), Space: 43 MB (21.50%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,21 +1,17 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        int lo= 0 , hi = arr.length -1, mid=0;
-        
-        while(lo <hi)
-        {   mid= (lo + hi)/2;
-            
-            if(arr[mid+1] < arr[mid] )
+         int lo= 0 , hi = arr.length -1, mi=0;
+        while(lo<hi)
+        {
+            mi= (lo+hi)/2;
+            if(arr[mi] > arr[mi+1])
             {
-                hi=mid;
+                hi=mi; /// means we are in the decresaing part of the array an it is a possible outcome 
             }
-            
-            else
-            {
-                lo=mid+1;
+            else{
+                lo=mi+1;  // means we are in the increasing part of the array an it cannot be the  possbile outocme
             }
         }
-        return lo;
-    
+        return lo;  //int the en start an en willl point to the same element due to the above checks
     }
 }
