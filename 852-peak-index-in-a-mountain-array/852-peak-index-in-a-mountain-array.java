1c188f1dfab36759518a99bc2b967a6e5869b5c9
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-02-26 10:55:26 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (31.98%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,4 +1,37 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-       int lo= 0 , hi = arr.length -1, mid=0;
-        while(lo <=hi){mid= (lo + hi)/2;if(arr[mid+1] < arr[mid] && arr[mid-1] < arr[mid]){return mid;}if(arr[mid+1] > arr[mid]){lo=mid+1;}else{hi=mid-1;}}return lo;}}
+       int lo= 0 , hi = arr.length -1, mi=0;
+        // //1st method
+        // while(lo <=hi)
+        // {   mid= (lo + hi)/2;
+        //     if(arr[mid+1] < arr[mid] && arr[mid-1] < arr[mid])
+        //     {
+        //         return mid;
+        //     }
+        //     if(arr[mid+1] > arr[mid])
+        //     {
+        //         lo=mid+1;
+        //     }
+        //     else
+        //     {
+        //         hi=mid-1;
+        //     }
+        // }
+        // return lo;
+        
+        //2nd method is same as above but checks are ifferent an less
+        
+        while(lo<hi)
+        {
+            mi= (lo+hi)/2;
+            if(arr[mi] > arr[mi+1])
+            {
+                hi=mi;
+            }
+            else{
+                lo=mi+1;
+            }
+        }
+        return lo;
+    }
+}
