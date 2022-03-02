3f5ac3a3331eab8b40abc4baaef12f7c4cae6511
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-03-02 16:52:54 +0530

    Time: 2 ms (45.06%), Space: 41.7 MB (42.22%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()==0)
+            return true;
+        int start =0;
+        int end=0;
+        while(end < t.length())
+        {
+            if(t.charAt(end)==s.charAt(start))
+            {
+                start++;
+                if(start==s.length())
+                    return true;
+            }
+            end++;
+        }
+        return false;
+        
+    }
+}
