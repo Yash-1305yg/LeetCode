cc2ffe62348cb4dcaf97df9c66703926688f92c1
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-03-04 19:11:35 +0530

    Time: 0 ms (100.00%), Space: 61.1 MB (8.55%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -2,7 +2,10 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int lo=0 ; int hi=nums.length-1;
         int len = nums.length;
-        
+        //here we are checking the length of right and left subarray
+        //if nums[mid] = nums[mid+1] then move mid to 1 behind
+        // then check the length of left subarray if it is odd then move hi to id
+        // else move to lo mid+1
         while(lo<hi )
         {
             int mid=(lo+hi)/2;
