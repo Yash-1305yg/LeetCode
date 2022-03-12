c9509ecb4f2ea09a01a4a2f348a5b695be32dffa
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-03-12 07:50:58 +0530

    Time: 22 ms (11.82%), Space: 42.9 MB (40.64%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,16 +10,52 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        ListNode temp=null;
-        while(current!=null){
-            temp=current.next;
-            current.next=prev;
-            prev=current;
-            current=temp;
+//         ListNode prev = null;
+//         ListNode current = head;
+//         ListNode temp=null;
+//         while(current!=null){
+//             temp=current.next;
+//             current.next=prev;
+//             prev=current;
+//             current=temp;
             
+//         }
+//         return prev;
+        if(head==null || head.next==null)return head;
+        int len=0;
+        ListNode curr=head;
+        while(curr.next!=null)
+        {
+            len++;
+            curr=curr.next;
         }
-        return prev;
+        int right= len;
+        int left = 1;
+        swap(head , left , right);
+        return head;
+    }
+    
+    public ListNode swap(ListNode head, int left , int right)
+    {
+        if(left>right)
+        {
+            return head; 
+        }
+        ListNode one = head;
+        ListNode two = head;
+        for(int i=1;i<left;i++)
+        {
+            one=one.next;
+        }
+        for(int i=1;i<=right;i++)
+        {
+            two=two.next;
+        }
+        
+        int temp=one.val;
+        one.val=two.val;
+        two.val=temp;
+        swap(head , left+1,right-1);
+        return head;
     }
 }
