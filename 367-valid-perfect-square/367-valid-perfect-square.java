686e3d0302e03501077ffa8e91e42e0fc5898250
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-04-01 17:33:10 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (44.27%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start=0;
+        int end=num;
+        if(num==1) return true;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(mid*mid==num )
+                return true;
+             if(mid > num / mid)
+            {
+                end=mid-1;
+            }
+            else
+                start=mid+1;
+        }
+        return false;
+    }
+}
