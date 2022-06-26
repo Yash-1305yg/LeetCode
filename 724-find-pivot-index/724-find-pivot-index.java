cdf8777b2cb85e6718bf903b88b75dfaeb02a49c
Yash Gupta <yash.gupta_cs19@gla.ac.in>
2022-06-26 08:46:33 +0530

    Time: 2 ms (75.17%), Space: 54.3 MB (5.68%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,29 +1,44 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalSum = 0;
-        for(int i:nums){
-            totalSum= totalSum+i;
-        }
-        int lo = 0;
-        int ans=0;
-        boolean flag = false;
-        for(int i =0;i<nums.length;i++){
-            int left=0;
-            int right = 0;
+//         My Solution
+//         int totalSum = 0;
+//         for(int i:nums){
+//             totalSum= totalSum+i;
+//         }
+//         int lo = 0;
+//         int ans=0;
+//         boolean flag = false;
+//         for(int i =0;i<nums.length;i++){
+//             int left=0;
+//             int right = 0;
             
-            for(int j=0 ; j<i ; j++){
-                left=left+nums[j];
+//             for(int j=0 ; j<i ; j++){
+//                 left=left+nums[j];
+//             }
+            
+//             right = totalSum - left - nums[i];
+//             if(right ==left)
+//                 {ans=i;
+//                 flag = true;
+//                 break;}
+//         }
+//         if(!flag){
+//             return -1;
+//         }
+//         return ans;
+        
+//         Optimized code
+        
+        int sum = 0 , left =0;
+        for(int i: nums){
+            sum+=i;
+        }
+        for(int i=0;i< nums.length;i++){
+            if(left == sum - nums[i] - left){
+                return i;
             }
-            
-            right = totalSum - left - nums[i];
-            if(right ==left)
-                {ans=i;
-                flag = true;
-                break;}
+            left = left + nums[i];
         }
-        if(!flag){
-            return -1;
-        }
-        return ans;
+        return -1;
     }
 }
